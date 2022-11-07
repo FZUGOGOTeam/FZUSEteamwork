1dd58bab50cd0fa86d51d1533123266b27300b21
leon2001 <1046465217@qq.com>
2022-11-07 17:40:19 +0800

    modify .gitignore

--- 后端/backend/src/main/java/com/gogo/web/SelectForeignByIdServlet.java
+++ 后端/backend/src/main/java/com/gogo/web/SelectForeignByIdServlet.java
@@ -1,17 +1,30 @@
 package com.gogo.web;
 
+import com.alibaba.fastjson.JSON;
+import com.gogo.pojo.DomesticPlayer;
+import com.gogo.pojo.ForeignPlayer;
+import com.gogo.service.DomesticPlayerService;
+import com.gogo.service.ForeignPlayerService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/selectForeignById")
 public class SelectForeignByIdServlet extends HttpServlet {
+    private final ForeignPlayerService foreignPlayerService = new ForeignPlayerService();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        String id = req.getParameter("id");
+        ForeignPlayer foreignPlayer = foreignPlayerService.selectById(Integer.parseInt(id));
+        String jsonString = JSON.toJSONString(foreignPlayer);
+        PrintWriter writer = resp.getWriter();
+        writer.write(jsonString);
+        writer.flush();
     }
 
     @Override
