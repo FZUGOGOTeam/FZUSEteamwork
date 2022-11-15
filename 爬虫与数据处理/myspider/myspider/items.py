b7574a006bb49df74c6e3d1504185ac3574ca006
Asayuri <39673419+achonic@users.noreply.github.com>
2022-11-15 17:59:15 +0800

    Get all native player data complete

--- 爬虫与数据处理/myspider/myspider/items.py
+++ 爬虫与数据处理/myspider/myspider/items.py
@@ -5,7 +5,7 @@
 
 import scrapy
 
-
+playerDataTableName = ['season', 'clubName', 'play_count', 'starter_count', 'goal', 'assists', 'yellow_card', 'red_card', '替补']
 class MyspiderItem(scrapy.Item):
     # define the fields for your item here like:
     player_name = scrapy.Field()
@@ -20,4 +20,12 @@ class MyspiderItem(scrapy.Item):
     player_birth = scrapy.Field()
     player_preferredfoot = scrapy.Field()
 
+    season = scrapy.Field()
+    clubName = scrapy.Field()
+    play_count = scrapy.Field()
+    starter_count = scrapy.Field()
+    goal = scrapy.Field()
+    assists = scrapy.Field()
+    yellow_card = scrapy.Field()
+    red_card = scrapy.Field()
     pass
--- 爬虫与数据处理/myspider/myspider/spiders/football.py
+++ 爬虫与数据处理/myspider/myspider/spiders/football.py
@@ -13,6 +13,9 @@ playerInfo = []
 tempPlayerInfo = []
 
 
+playerDataTableName = ['season', 'clubName', 'play_count', 'starter_count', 'goal', 'assists', 'yellow_card', 'red_card']
+
+
 def isU21(str):
     return "U21" in str
 
@@ -78,7 +81,25 @@ def GetUnder21And23Player(script):
 class FootballSpider(scrapy.Spider):
     name = 'football'
     allowed_domains = ['www.dongqiudi.com']
-    start_urls = ['https://www.dongqiudi.com/team/50077677.html']
+    start_urls = [
+      #  'https://www.dongqiudi.com/team/50000335.html',
+      #  'https://www.dongqiudi.com/team/50077677.html',
+      #  'https://www.dongqiudi.com/team/50007190.html',
+      #  'https://www.dongqiudi.com/team/50000341.html'
+        'https://www.dongqiudi.com/team/50000339.html',
+        'https://www.dongqiudi.com/team/50000330.html',
+        'https://www.dongqiudi.com/team/50076899.html',
+        'https://www.dongqiudi.com/team/50000334.html',
+        'https://www.dongqiudi.com/team/50014906.html',
+        'https://www.dongqiudi.com/team/50000332.html',
+        'https://www.dongqiudi.com/team/50000337.html',
+        'https://www.dongqiudi.com/team/50010891.html',
+        'https://www.dongqiudi.com/team/50012222.html',
+        'https://www.dongqiudi.com/team/50009305.html',
+        'https://www.dongqiudi.com/team/50012355.html',
+        'https://www.dongqiudi.com/team/50004829.html'
+
+                  ]
     #/team/50077677.html
 
 
@@ -102,6 +123,25 @@ class FootballSpider(scrapy.Spider):
 
 
     def parse_second(self, response):   #每一个球员的爬取
+        items = MyspiderItem()
+        data = response.css(".total-con-wrap .th span::text").extract()
+        print(data)
+
+        playername = response.xpath("string(//p[@class='china-name'])").get()
+
+        data = response.css(".total-con-wrap .td span::text").extract()
+        print(type(data))
+        print(data)
+
+
+        for i in range(0, len(data)):
+            if i % 9 == 8:
+                items['player_name'] = playername
+                yield items
+                continue
+            items[playerDataTableName[i%9]] = data[i]
+
+
 
         data = response.xpath("string(//div[@class='player-info'])").get()
         print(data)
@@ -124,7 +164,10 @@ class FootballSpider(scrapy.Spider):
 
         playerInfo.append({"name": tempPlayerInfo[9],"Picsrc": tempPlayerInfo[10],"age":tempPlayerInfo[4],"birthday": tempPlayerInfo[7], "clubName": tempPlayerInfo[0], "country": tempPlayerInfo[1],
                            "height":tempPlayerInfo[2],"number":tempPlayerInfo[6],"position":tempPlayerInfo[3],"preferredFoot": tempPlayerInfo[8],"weight":tempPlayerInfo[5]})
-        items = MyspiderItem()
+        '''
+        for i in tempPlayerInfo:
+            if i == "":
+                i = "null"
         items['player_name'] = tempPlayerInfo[9]
         items['player_photo'] = tempPlayerInfo[10]
         items['player_club'] = tempPlayerInfo[0]
@@ -137,4 +180,5 @@ class FootballSpider(scrapy.Spider):
         items['player_birth'] = tempPlayerInfo[7]
         items['player_preferredfoot'] = tempPlayerInfo[8]
         yield items
-        print(playerInfo)
+        '''
+     #   print(playerInfo)
