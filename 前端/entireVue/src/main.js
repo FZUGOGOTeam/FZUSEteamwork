817f012b3d093690a2d2e3ca01af5cf200c5a3bb
MoonlessLin <112751876+MoonlessLin@users.noreply.github.com>
2022-12-18 19:43:13 +0800

    在main.js中引入element-plus

--- 前端/entireVue/src/main.js
+++ 前端/entireVue/src/main.js
@@ -3,4 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+createApp(App).use(store).use(ElementPlus).use(router).mount('#app')
+
+
